6520705a21ab9c8fe22a55e0a7160bac49f0707a
Yurii Lopin <yurii.lopin@pm.me>
2022-04-29 17:18:45 +0300

    Fixes.

--- conftest.py
+++ conftest.py
@@ -3,12 +3,14 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.firefox import GeckoDriverManager
 
 @pytest.fixture(autouse=True)
-def test_setDriver():
+def Driver():
     # global driver
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+    driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
     yield driver
+    driver.close()
     # driver.get("https://secure.sitelock.com/login")
     # driver.maximize_window()
     
